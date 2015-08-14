8bfd008a3f759a4cffed8b8ec89ba238914831f5
CorrieASmith <corriesmith87@gmail.com>
2015-08-14 09:31:01 -0700

    spec will return a synonym and antonym of the word

--- dictionary/lib/definition.rb
+++ dictionary/lib/definition.rb
@@ -1,11 +1,21 @@
 class Definition
   @@definition = []
 
-  define_method(:initialize) do |meaning|
+  define_method(:initialize) do |meaning, synonym, antonym|
     @meaning = meaning
+    @synonym = synonym
+    @antonym = antonym
   end
 
   define_method(:meaning) do
     @meaning
+  end
+
+  define_method(:synonym) do
+    @synonym
+  end
+
+  define_method(:antonym) do
+    @antonym
   end
 end
--- dictionary/spec/definition_spec.rb
+++ dictionary/spec/definition_spec.rb
@@ -6,8 +6,22 @@ describe('Definition') do
 
   describe('#meaning') do
     it("returns the meaning of the word") do
-      test_definition = Definition.new("not able to be corrected")
+      test_definition = Definition.new("not able to be corrected", "habitiual", "repentant")
       expect(test_definition.meaning()).to(eq("not able to be corrected"))
+    end
+  end
+
+  describe('#synonym') do
+    it("returns a synonym of the word") do
+      test_definition = Definition.new("not able to be corrected", "habitual", "repentant")
+      expect(test_definition.synonym()).to(eq("habitual"))
+    end
+  end
+
+  describe('#antonym') do
+    it("returns an antonym of the word") do
+      test_definition = Definition.new("not able to be corrected", "habitiual", "repentant")
+      expect(test_definition.antonym()).to(eq("repentant"))
     end
   end
 end
